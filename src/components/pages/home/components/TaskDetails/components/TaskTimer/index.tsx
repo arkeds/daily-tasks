cd715c2357a6ffc2f0ec8c72d7615e2399839b99
Arvin Kenn delos Santos <adsantos.417@gmail.com>
2023-02-02 14:31:31 +0800

    Created TaskTimer component

---
+++ src/components/pages/home/components/TaskDetails/components/TaskTimer/index.tsx
@@ -0,0 +1,11 @@
+import { Paper } from "@mui/material";
+import styles from "./tasktimer.module.scss";
+const TaskTimer = () => {
+  return (
+    <Paper className={styles.task_timer_container} elevation={3}>
+      Task
+    </Paper>
+  );
+};
+
+export default TaskTimer;
