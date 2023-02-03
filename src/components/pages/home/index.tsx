b42d502a5dae79c5461eff005e1ae2bbe96eac43
Arvin Kenn delos Santos <adsantos.417@gmail.com>
2023-02-03 09:53:21 +0800

    Wrap Home with TaskContextProvider

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -3,17 +3,20 @@ import TaskDetails from "./components/TaskDetails";
 import TaskDialogue from "./components/TaskDialogue";
 import TasksList from "./components/TasksList";
 import { DialogueContextProvider } from "./context/DialogContext";
+import { TaskContextProvider } from "./context/TasksContext";
 import style from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <DialogueContextProvider>
-        <Grid container className={style.daily_tasks_home}>
-          <TasksList />
-          <TaskDetails />
-        </Grid>
-        <TaskDialogue />
+        <TaskContextProvider>
+          <Grid container className={style.daily_tasks_home}>
+            <TasksList />
+            <TaskDetails />
+          </Grid>
+          <TaskDialogue />
+        </TaskContextProvider>
       </DialogueContextProvider>
     </>
   );
